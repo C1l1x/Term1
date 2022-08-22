a45258f74acfdd8f4412a343122c9cf072b07515
C1l1x <pavlov_s@abv.bg>
2022-08-22 20:14:43 +0300

    init

--- dom_1.cpp
+++ dom_1.cpp
@@ -7,7 +7,7 @@ int main()
         int i = 0, S = 0;
         do (i = i + 2, S = S + i);
         while (S < 1000);
-        cout << S << endl;
+        cout << "Answer is " << S << endl;
         system("pause");
     }
 }
